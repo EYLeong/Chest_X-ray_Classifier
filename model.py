a3c7174624862847f1ac4c65fbda74625ff68679
Soo Han <soohanson@gmail.com>
2021-03-16 02:27:09 +0800

    added save, load of ML models, and created a combined model class that joins these two models

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ from torch import optim, nn
 import torch.nn.functional as F
 from tqdm.notebook import tqdm
 import copy
+import numpy as np
 
 class CNN(nn.Module):
     def __init__(self):
@@ -140,7 +141,7 @@ def validate(model, device, loss_criterion, val_loader):
     
     return (val_loss / len(val_loader))
     
-def test(model, device, test_loader, plot=False):
+def test(model, device, test_loader):
     model.eval()
     
     correct = 0
@@ -153,8 +154,100 @@ def test(model, device, test_loader, plot=False):
             
             output = model.forward(data)
             pred = torch.round(output)
-
             equal_data = torch.sum(target.data == pred).item()
             correct += equal_data
 
     print('Test set accuracy: ', 100. * correct / len(test_loader.dataset), '%')
+
+def save_model(model, optimizer, path):
+    checkpoint = {'state_dict': model.state_dict(),
+                  'opti_state_dict': optimizer.state_dict()
+                  }
+    torch.save(checkpoint, path)
+
+def load_model(model, optimizer, path, device):
+    '''
+    Saves model parameters instead of the whole model. 
+    Requires first initialization of the model class to be fed as input to this function, in which
+    state_dictionaries are loaded and returned
+    '''
+
+    checkpoint = torch.load(path)
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer = optimizer.load_state_dict(checkpoint['opti_state_dict'])
+    
+    return model, optimizer
+
+class Combined_Model:
+    def __init__(self,fl_model,sl_model):
+        self.fl_model = fl_model
+        self.sl_model = sl_model
+
+    def test_combine(self, device, test_loader):
+
+        #Currently using first layer test loader for predictions for BOTH first and second layer
+        #models. Somehow the combined test loader's input faces some issues
+
+        self.fl_model.eval()
+        self.sl_model.eval()
+
+        correct = 0
+
+        with torch.no_grad():
+            for data, target in test_loader:
+
+                data, target = data.to(device), target.to(device)
+                
+                fl_output = self.fl_model.forward(data)
+                fl_pred = torch.round(fl_output)
+
+                sl_output = self.sl_model.forward(data)
+                sl_pred = torch.round(sl_output)
+
+                # method to split the fl target and sl targets
+                fl_target = target[:,:1]
+                sl_target_covid = target[:,1:2] 
+                sl_target_noncovid = target[:,2:3]  
+                
+
+                #First Layer Check 
+                #for normal cases we manipulate so that 0 becomes 1, and 1 becomes 0           
+                fl_pred_mod = copy.deepcopy(fl_pred)
+                fl_pred_mod[fl_pred_mod==0] = 2
+                fl_pred_mod[fl_pred_mod==1] = 0
+                fl_pred_mod[fl_pred_mod==2] = 1
+
+                equal_data_fl = torch.sum(torch.mul(fl_target.data,fl_pred_mod)).item()
+                correct += equal_data_fl
+
+                #Second Layer Check 
+                #we copy another target tensor for covid cases and manipulate so that 0 becomes 1, and 1 becomes 0
+                sl_pred_covid = copy.deepcopy(sl_pred)             
+                sl_pred_covid[sl_pred_covid==0] = 2
+                sl_pred_covid[sl_pred_covid==1] = 0
+                sl_pred_covid[sl_pred_covid==2] = 1
+
+                sl_pred_covid = torch.mul(fl_pred,sl_pred_covid)
+                sl_pred_non_covid = torch.mul(fl_pred,sl_pred)
+                
+                equal_data_sl_covid = torch.sum(torch.mul(sl_target_covid.data,sl_pred_covid)).item()
+                equal_data_sl_non_covid = torch.sum(torch.mul(sl_target_noncovid.data,sl_pred_non_covid)).item()
+                     
+                correct += equal_data_sl_covid
+                correct += equal_data_sl_non_covid
+
+        print('Test set accuracy: ', 100. * correct / len(test_loader.dataset), '%')
+
+
+
+def combine_models(fl_image_path, sl_image_path, device, L_RATE):
+    
+    fl_model = CNN().to(device)
+    fl_optimizer = optim.Adam(fl_model.parameters(), lr=L_RATE)
+    fl_model, _ = load_model(fl_model, fl_optimizer, fl_image_path, device)
+    
+    sl_model = CNN().to(device)
+    sl_optimizer = optim.Adam(sl_model.parameters(), lr=L_RATE)
+    sl_model, _ = load_model(sl_model, sl_optimizer, sl_image_path, device)
+
+    return Combined_Model(fl_model, sl_model)
